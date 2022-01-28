f17563d7adb0e12eed74ddfe463cedc7cd6c4579
Shiyasmohd <shiyasjaseena124@gmail.com>
2022-01-28 23:03:38 +0530

    Details top finished

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Details.css'
 import BG from '../../Assets/BG.jpg'
+import Donate from '../../Assets/donate.svg'
+import Share from '../../Assets/share.svg'
 
 function Details() {
     var sectionStyle={
@@ -13,10 +15,24 @@ function Details() {
                   <div className="top-details">
                     <h1>Earthquake - Kerala</h1>
                     <h4>Reported By John Abhraham</h4>
+                    <div className="donate">
+                        <div className="d-btn">
+                            <img src={Donate} />
+                            <p>Donate</p>
+                        </div>
+                        <div className="s-btn">
+                        <img src={Share} />
+                        <p>Share</p>
+                        </div>
+                    </div>
                   </div>
               </div>
               <div className="bottom-wrap">
                 <div className="bottom-details">
+                    <div className="b-details-head">
+                        <h1>Kerala</h1>
+                        <h4>South West Region</h4>
+                    </div>
                 </div>
               </div>
               
